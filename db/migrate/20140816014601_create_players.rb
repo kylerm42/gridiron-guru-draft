b413d9e2f9c6c53bcff7e871d121932f75c10c16
Kyle Moore <kylerm42@gmail.com>
2014-08-16 01:00:14 -0400

    added player seeding

--- db/migrate/20140816014601_create_players.rb
+++ db/migrate/20140816014601_create_players.rb
@@ -7,11 +7,12 @@ class CreatePlayers < ActiveRecord::Migration
       t.string   :team,          null: false, default: 'FA'
       t.date     :birthdate
       t.string   :college
-      t.integer  :height
+      t.string   :height
       t.integer  :weight
       t.integer  :years_pro
       t.integer  :profile_id
       t.string   :gsis_id
+      t.integer  :rank
 
       t.integer  :pass_yards,    default: 0
       t.integer  :pass_tds,      default: 0
@@ -23,20 +24,22 @@ class CreatePlayers < ActiveRecord::Migration
       t.integer  :rec_yards,     default: 0
       t.integer  :rec_tds,       default: 0
       t.integer  :fumbles,       default: 0
-      t.integer  :two_pt_conv,   default: 0
+      t.integer  :pass_2_pt,     default: 0
+      t.integer  :rush_2_pt,     default: 0
+      t.integer  :rec_2_pt,      default: 0
 
       t.integer  :fg_made_yards, default: 0
       t.integer  :fg_miss_yards, default: 0
       t.integer  :made_pat,      default: 0
       t.integer  :miss_pat,      default: 0
-      t.integer  :made_20,       default: 0
-      t.integer  :miss_20,       default: 0
-      t.integer  :made_30,       default: 0
-      t.integer  :miss_30,       default: 0
-      t.integer  :made_40,       default: 0
-      t.integer  :miss_40,       default: 0
-      t.integer  :made_50,       default: 0
-      t.integer  :miss_50,       default: 0
+      t.integer  :made_0_19,     default: 0
+      t.integer  :miss_0_19,     default: 0
+      t.integer  :made_20_29,    default: 0
+      t.integer  :miss_20_29,    default: 0
+      t.integer  :made_30_39,    default: 0
+      t.integer  :miss_30_39,    default: 0
+      t.integer  :made_40_49,    default: 0
+      t.integer  :miss_40_49,    default: 0
       t.integer  :made_50_plus,  default: 0
       t.integer  :miss_50_plus,  default: 0
 
@@ -54,6 +57,6 @@ class CreatePlayers < ActiveRecord::Migration
 
     add_index :players, :position
     add_index :players, [:last_name, :first_name]
-    add_index :players, :nfl_team
+    add_index :players, :team
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,14 +36,15 @@ ActiveRecord::Schema.define(version: 20140816014601) do
     t.string   "first_name",                   null: false
     t.string   "last_name",                    null: false
     t.string   "position",                     null: false
-    t.string   "nfl_team",      default: "FA", null: false
+    t.string   "team",          default: "FA", null: false
     t.date     "birthdate"
     t.string   "college"
-    t.integer  "height"
+    t.string   "height"
     t.integer  "weight"
     t.integer  "years_pro"
     t.integer  "profile_id"
     t.string   "gsis_id"
+    t.integer  "rank"
     t.integer  "pass_yards",    default: 0
     t.integer  "pass_tds",      default: 0
     t.integer  "pass_ints",     default: 0
@@ -54,17 +55,21 @@ ActiveRecord::Schema.define(version: 20140816014601) do
     t.integer  "rec_yards",     default: 0
     t.integer  "rec_tds",       default: 0
     t.integer  "fumbles",       default: 0
-    t.integer  "two_pt_conv",   default: 0
+    t.integer  "pass_2_pt",     default: 0
+    t.integer  "rush_2_pt",     default: 0
+    t.integer  "rec_2_pt",      default: 0
+    t.integer  "fg_made_yards", default: 0
+    t.integer  "fg_miss_yards", default: 0
     t.integer  "made_pat",      default: 0
     t.integer  "miss_pat",      default: 0
-    t.integer  "made_20",       default: 0
-    t.integer  "miss_20",       default: 0
-    t.integer  "made_30",       default: 0
-    t.integer  "miss_30",       default: 0
-    t.integer  "made_40",       default: 0
-    t.integer  "miss_40",       default: 0
-    t.integer  "made_50",       default: 0
-    t.integer  "miss_50",       default: 0
+    t.integer  "made_0_19",     default: 0
+    t.integer  "miss_0_19",     default: 0
+    t.integer  "made_20_29",    default: 0
+    t.integer  "miss_20_29",    default: 0
+    t.integer  "made_30_39",    default: 0
+    t.integer  "miss_30_39",    default: 0
+    t.integer  "made_40_49",    default: 0
+    t.integer  "miss_40_49",    default: 0
     t.integer  "made_50_plus",  default: 0
     t.integer  "miss_50_plus",  default: 0
     t.integer  "sacks",         default: 0
@@ -72,15 +77,15 @@ ActiveRecord::Schema.define(version: 20140816014601) do
     t.integer  "fum_rec",       default: 0
     t.integer  "safeties",      default: 0
     t.integer  "def_tds",       default: 0
-    t.integer  "ret_tds",       default: 0
+    t.integer  "return_tds",    default: 0
     t.integer  "pts_allowed",   default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "players", ["last_name", "first_name"], name: "index_players_on_last_name_and_first_name", using: :btree
-  add_index "players", ["nfl_team"], name: "index_players_on_nfl_team", using: :btree
   add_index "players", ["position"], name: "index_players_on_position", using: :btree
+  add_index "players", ["team"], name: "index_players_on_team", using: :btree
 
   create_table "sessions", force: true do |t|
     t.string   "token"
